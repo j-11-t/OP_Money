beadc13cf74d167ba7882f7629cdf5cade3b9a59
j-11-t <nursing-dodgy-ploy@duck.com>
2024-08-25 20:32:46 -0700

    7

--- AFK_OP_Money.lua
+++ AFK_OP_Money.lua
@@ -2,7 +2,7 @@
 
 
 local response = false
-local localVer = 4.3
+local localVer = 4.4
 local scriptName = "AFK_OP_Money"
 local versionCheckInterval = 300000 -- 5 minutos
 local updateButtonCreated = false
